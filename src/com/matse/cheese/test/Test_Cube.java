6da27f08f148e2cd9f955bfd2e7a2f7faca57591
Olga Müller <olga.mueller@yahoo.de>
2015-09-30 12:41:54 +0200

    add new Tests

--- src/com/matse/cheese/test/Test_Cube.java
+++ src/com/matse/cheese/test/Test_Cube.java
@@ -1,10 +1,32 @@
-package src.com.matse.cheese.test;
-import src.com.matse.cheese.bean.Cube;
-import src.com.matse.cheese.bean.Segment;
-import src.com.matse.cheese.bean.SegmentType;;
+package com.matse.cheese.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.matse.cheese.bean.Cube;
+import com.matse.cheese.bean.Segment;
+import com.matse.cheese.bean.SegmentType;
+import com.matse.cheese.solver.RecursiveSolver;
 
 public class Test_Cube {
 
+	private boolean compareList(List<Segment> erwarteterWeg, List<Segment> weg) {
+		if (erwarteterWeg.size() != weg.size())
+			return false;
+
+		for (int i = 0; i != erwarteterWeg.size(); i++) {
+			if (!weg.get(i).getType().equals(erwarteterWeg.get(i).getType())) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Test
 	public void testStartsegment() {
 		Segment[][][] segments = new Segment[3][3][3];
 		for (int x = 0; x < 3; x++)
@@ -35,6 +57,26 @@ public class Test_Cube {
 				}
 		assertEquals("Mehr als 3 Nachbar gefunden", 3, cube.getNeighbours(segments[0][0][0]).size());
 		assertEquals("Valide Nachbar gefunden", 0, cube.getValidNeighbours(segments[0][0][2]).size());
+
+		for (int x = 0; x < 3; x++)
+			for (int y = 0; y < 3; y++)
+				for (int z = 0; z < 3; z++) {
+					segments[x][y][z] = new Segment(SegmentType.CHEESE, false, x, y, z);
+				}
+		for (int z = 0; z < 3; z++)
+			segments[1][1][z].setType(SegmentType.AIR);
+
+		Cube kaese = new Cube(segments);
+
+		RecursiveSolver solver = new RecursiveSolver(kaese);
+
+		assertTrue("Kein Weg gefunden", solver.solve());
+		assertEquals("Die Länge stimmt nicht", 3, solver.getPathLength());
+
+		List<Segment> erwarteterWeg = Arrays.asList(segments[1][1][0], segments[1][1][1], segments[1][1][2]);
+		List<Segment> weg = solver.searchPath(-1);
+		assertEquals("Die Länge stimmt nicht überein", erwarteterWeg.size(), weg.size());
+		assertTrue("Falscher Weg", compareList(erwarteterWeg, weg));
 
 	}
 
